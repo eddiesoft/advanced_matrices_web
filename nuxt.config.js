29fd9cc798343a3e1c977018173caa0619fb57d5
adiletbeishekeev98 <adiletbeishekeev98@gmail.com>
2022-07-14 02:42:03 +0600

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      { rel: 'icon', type: 'image/png', href: '/advanced_matrices_web/favicon.png' },
       {
         rel: 'stylesheet',
         href: 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css'
